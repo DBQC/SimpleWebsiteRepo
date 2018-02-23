5c0635b460fae55e37a28d81d7e2b191692d9ea2
CHRIS-DB\Chris <cstep1990@gmail.com>
2018-02-23 18:25:25 +0000

    Add project files.

---
+++ SimpleWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleWebsite.Startup))]
+namespace SimpleWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
